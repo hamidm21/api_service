6a5a2be48a43711607a1ec0329f27e269b513a6a
Hamid <hordimad21@gmail.com>
2019-02-05 10:54:59 +0330

    __CHANGE__ change in database

--- routes/User.js
+++ routes/User.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const controller = require('../lib/controllers/user_controller');
 
+router.post('/extraInfo', controller.extraInfo);
 
-router.get('/' , ()=> {
-
-});
+router.post('/getProfile', controller.getProfile);
 
 module.exports = router;
